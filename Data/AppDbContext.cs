bfadd21437ff57747c483f5d71562a9016e784aa
Leo Pirringer <leo.pirringer@gmail.com>
2026-06-10 14:18:54 +0200

    Add AppDbContext class for database context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,5 +3,12 @@ using TodoAppApi.Models;
 
 namespace TodoAppApi.Data;
 
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
     public DbSet<Todo> Todos { get; set; }
 }
